a57d966f331a55d3e79c3d5a8dac206d46ed3262
Gil Beyruth <firstpixel@gmail.com>
2014-09-02 00:43:09 -0300

    inital commit

--- TWHashTag/ViewController.m
+++ TWHashTag/ViewController.m
@@ -7,18 +7,60 @@
 //
 
 #import "ViewController.h"
+#import "STTwitter.h"
 
-@interface ViewController ()
+@interface ViewController () <UITableViewDelegate, UITableViewDataSource>
+
+    @property (weak, nonatomic) IBOutlet UITableView *twitterTableView;
+    @property (strong, nonatomic) NSMutableArray *twitterFeed;
 
 @end
 
 @implementation ViewController
 
+@synthesize twitterFeed;
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    STTwitterAPI* twitter = [STTwitterAPI twitterAPIAppOnlyWithConsumerKey:@"iqUxUUKT4GnEpKZqdZhrlOEct" consumerSecret:@"UebKOVbYPf7Zm4NaMUaK8fLsEeuLk5gIfUyQk27JzvmEb9AIav"];
+    
+    [twitter verifyCredentialsWithSuccessBlock:^(NSString *username) {
+        [twitter getSearchTweetsWithQuery:@"%23vagas" successBlock:^(NSDictionary *searchMetadata, NSArray *statuses) {
+            self.twitterFeed = [NSMutableArray arrayWithArray:statuses];
+            [self.twitterTableView reloadData];
+        } errorBlock:^(NSError *error) {
+            NSLog(@"Search Error :%@", error);
+        }];
+    } errorBlock:^(NSError *error) {
+        NSLog(@"Credential Error :%@", error);
+    }];
+    
 }
+
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return self.twitterFeed.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath;
+{
+    
+    UITableViewCell* cell = [tableView dequeueReusableCellWithIdentifier:@"CellID"];
+    if(cell == nil){
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"CellID"];
+        
+    }
+    
+    NSInteger index = indexPath.row;
+    NSDictionary *t = self.twitterFeed[index];
+    cell.textLabel.text = t[@"text"];
+    
+    return cell;
+}
+
 
 - (void)didReceiveMemoryWarning
 {
